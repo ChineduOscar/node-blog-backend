d80d78ae54e216f3bf51cffecd97adf0378c2f3d
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2024-12-23 22:01:28 -0800

    adding swagger UI

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -40,6 +45,10 @@ app.use(cors());
 app.use(xss());
 app.use(fileUpload({ useTempFiles: true }));
 
+app.get('/', (req, res) => {
+  res.send('<h1>Blog Posts API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/posts', postRouter);
@@ -47,11 +56,7 @@ app.use('/api/v1/posts', postRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Post API</h1>');
-});
-
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
